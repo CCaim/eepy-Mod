f85e81407c0e15f898f30b96642501a879785fdb
CCaim <barreirocarlos2001@gmail.com>
2024-06-04 14:40:11 +0200

    Creation of the eepy mod project

---
+++ src/main/java/net/caim/eepy/EepyClient.java
@@ -0,0 +1,11 @@
+package net.caim.eepy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EepyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
